2c4fb53a47699baa53bfe71b3897f832ed7296e0
adel <adel1@outlook.com>
2026-02-08 14:52:23 +0300

    Your commit message

---
+++ fiveserver.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  port: 1001,
+  root: ".",
+  open: "index.html"
+};
+
+
+
+// https://www.essentialdesigns.net/?utm_source=clutch.co&utm_medium=referral_profile&utm_campaign=web-developers
+// https://geniusee.com/?utm_source=Clutch&utm_medium=referral&utm_campaign=Sponsored_Top_Web_Development_Companies
+// https://www.element8.sa/
+// https://www.element8.sa/web-design-company-saudi-arabia?utm_source=Google%20Search&utm_medium=CPC&utm_campaign=Webdesign%20Leads&utm_term=best%20website%20designers&utm_campaign=Ele8-+Saudi-ACQ-PSEARCH-CPC-TAD-OTH-EXTMT-GOOGL-GI&utm_source=adwords&utm_medium=ppc&hsa_acc=3855311896&hsa_cam=21055596697&hsa_grp=159701669656&hsa_ad=691912271020&hsa_src=g&hsa_tgt=kwd-12934446&hsa_kw=best%20website%20designers&hsa_mt=b&hsa_net=adwords&hsa_ver=3&gad_source=1&gad_campaignid=21055596697&gbraid=0AAAAAqngx7KP_JoDVlbuwUv77zlx9Oeck&gclid=CjwKCAiAs4HMBhBJEiwACrfNZRLLEn2hLCNh1HOcMkuVXk9dp5yG3JHvfQQgALWsOc8QLS1EcsflyxoCXBkQAvD_BwE
---
+++ js/users.js
@@ -0,0 +1,75 @@
+// users.js
+window.TASHER_USERS = [
+
+  // 1️⃣ Branches Operations
+  { username: "branches_ops", password: "ta@2025", role: "department", deptCode: "branches_ops", displayName: "Branches Operations" },
+
+  // 2️⃣ Business Development
+  { username: "business_dev", password: "ta@2025", role: "department", deptCode: "business_dev", displayName: "Business Development" },
+
+  // 3️⃣ Business Excellence Department
+  { username: "business_excellence", password: "ta@2025", role: "department", deptCode: "business_excellence", displayName: "Business Excellence Department" },
+
+  // 4️⃣ Corporate IT
+  { username: "corporate-IT", password: "ta@2025", role: "department", deptCode: "corporate_it", displayName: "Corporate IT" },
+
+  // 5️⃣ Cybersecurity Department
+  { username: "cybersecurity", password: "ta@2025", role: "department", deptCode: "cybersecurity", displayName: "Cybersecurity Department" },
+
+  // 6️⃣ Data Analytics Department
+  { username: "data_analytics", password: "ta@2025", role: "department", deptCode: "data_analytics", displayName: "Data Analytics Department" },
+
+  // 7️⃣ Finance Department
+  { username: "finance", password: "ta@2025", role: "department", deptCode: "finance", displayName: "Finance Department" },
+
+  // 8️⃣ GRC Department
+  { username: "grc", password: "ta@2025", role: "department", deptCode: "grc", displayName: "GRC Department" },
+
+  // 9️⃣ Human Capital Department
+  { username: "human_capital", password: "ta@2025", role: "department", deptCode: "human_capital", displayName: "Human Capital Department" },
+
+  // 🔟 Infrastructure and Network Operations
+  { username: "infra-ops", password: "ta@2025", role: "department", deptCode: "infra-ops", displayName: "Infrastructure and Network Operations" },
+
+  // 1️⃣1️⃣ Internal Audit Department
+  { username: "internal_audit", password: "ta@2025", role: "department", deptCode: "internal_audit", displayName: "Internal Audit Department" },
+
+  // 1️⃣2️⃣ IT Solution Operations
+  { username: "IT-Operations", password: "ta@2025", role: "department", deptCode: "IT-Operations", displayName: "Platforms & IT Solution Operations" },
+
+  // 1️⃣3️⃣ Legal & Regulatory Affairs Department
+  { username: "legal_affairs", password: "ta@2025", role: "department", deptCode: "legal_affairs", displayName: "Legal & Regulatory Affairs Department" },
+
+  // 1️⃣4️⃣ Marketing & Communications
+  { username: "marketing", password: "ta@2025", role: "department", deptCode: "marketing", displayName: "Marketing & Communications" },
+
+  // 1️⃣5️⃣ PMO
+  { username: "pmo", password: "ta@2025", role: "department", deptCode: "pmo", displayName: "PMO" },
+
+  // 1️⃣6️⃣ Procurement Department
+  { username: "procurement", password: "1", role: "department", deptCode: "procurement", displayName: "Procurement Department" },
+
+  // 1️⃣7️⃣ Product Management
+  { username: "product_mgmt", password: "ta@2025", role: "department", deptCode: "product_mgmt", displayName: "Product Management" },
+
+  // 1️⃣8️⃣ SAP Department
+  { username: "sap", password: "sap", role: "department", deptCode: "sap_ERP", displayName: "SAP ERP Department" },
+
+  // 1️⃣9️⃣ Solution Development & Delivery
+  { username: "solution_delivery", password: "ta@2025", role: "department", deptCode: "solution_delivery", displayName: "Solution Development & Delivery" },
+
+  // 2️⃣0️⃣ Tech Strategy & Enterprise Architecture
+  { username: "onclick", password: "ta@2025", role: "department", deptCode: "tech_ITSM", displayName: "Tech Strategy & Enterprise Architecture" },
+  
+
+  // 2️⃣1️⃣ Tech Strategy & Enterprise Architecture
+  { username: "tech_strategy", password: "ta@2025", role: "department", deptCode: "tech_strategy", displayName: "Tech Strategy & Enterprise Architecture" },
+
+  // 🅰️ Admin Account
+  { username: "admin", password: "ta@2025", role: "admin", deptCode: "admin", displayName: "DMI Administrator" }
+
+
+];
+
+
+window.EVIDENCE_SHARED_FOLDER = "https://tasheer.sharepoint.com/sites/DMI/Shared%20Documents";
